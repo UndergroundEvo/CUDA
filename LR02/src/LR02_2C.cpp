1b21ddc91c9f6719da094a60494b8f1f2e050689
Miron <popovmiron@gmail.com>
2024-02-18 16:43:32 +0700

    Настройка сборки для cmake добавление файлов для Lab3

---
+++ LR02/src/LR02_2C.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+using namespace std;
+
+const int n = 10000;
+
+void vectorAdd(const vector<float> &a, const vector<float> &b, vector<float> &c, int start, int end) {
+    for (int i = start; i < end; ++i) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+int main() {
+    vector<float> a(n), b(n), c(n);
+    int numThreads = thread::hardware_concurrency();
+
+    for (int i = 0; i < n; ++i) {
+        a[i] = rand()%9999999 +1;;
+        b[i] = rand()%9999999 +1;;
+    }
+
+    vector<thread> threads;
+    for (int i = 0; i < numThreads; ++i) {
+        int start = i * (n / numThreads);
+        int end = (i == numThreads - 1) ? n : (i + 1) * (n / numThreads);
+        threads.emplace_back(vectorAdd, ref(a), ref(b), ref(c), start, end);
+    }
+
+    for (auto &thread : threads) {
+        thread.join();
+    }
+
+    cout << "Result: ";
+    for (int i = 0; i < n; ++i) {
+        cout << c[i] << " \n";
+    }
+    cout << endl;
+    return 0;
+}
