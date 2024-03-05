3b3b07d0e7993d7afa2838bfc383d2386d4cef13
Miron <popovmiron@gmail.com>
2024-03-06 00:10:12 +0700

    LR04 надеюсь финал

--- LR04/src/LR04_1G.cu
+++ LR04/src/LR04_1G.cu
@@ -1,93 +1,82 @@
+#include <cuda.h>
 #include <iostream>
-#include <chrono>
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
 using namespace std;
+// https://habr.com/ru/articles/54707/
 
-const unsigned int N = 10;
-const unsigned int K = 10;
-const unsigned int BANK_SIZE = 32;
-const unsigned int blockSize = 1024;
-const unsigned int numBlocks = 2048;
-
-typedef std::chrono::milliseconds ms;
-typedef std::chrono::nanoseconds ns;
-
-__global__ void MatrixFill(int *a1) {
-    unsigned int k = blockDim.x * blockIdx.x + threadIdx.x;
-    unsigned int n = blockDim.y * blockIdx.y + threadIdx.y;
-//  if (k >= N || n >= K) return;
-//  a1[k + N * n] = k + N * n;
-//  for (int i=0;i<N;i++) a1[k + N * n] = i;
-    a1[k + N * n] = k + N * n;
-}
-__global__ void TransMatrix(int *a1, int *a2) {
-    unsigned int k = blockDim.x * blockIdx.x + threadIdx.x;
-    unsigned int n = blockDim.y * blockIdx.y + threadIdx.y;
-    if (k >= N || n >= K) return;
-    a2[n + K * k] = a1[k + N * n];
+__global__ void gTransposition(int *a, int *b, int N, int K) {
+    //extern __shared__ int B[];
+    unsigned int k = threadIdx.x + blockIdx.x * blockDim.x;
+    unsigned int n = threadIdx.y + blockIdx.y * blockDim.y;
+    b[n + k * N] = a[k + n * K];
+    //B[n + k * N] = a[k + n * K];
+    //__syncthreads();
+    //b[k + n * N] = B[k + n * N];
 }
 
 int main() {
+    const int N = 4, K = 8, threads_per_block = 4;
     float elapsedTime;
+    int *GPU_pre_matrix, *local_pre_matrix, *GPU_after_matrix, *local_after_matrix;
     cudaEvent_t start, stop;
-    chrono::time_point<chrono::system_clock> start_chrono, end_chrono;
-
-    int *a1, *a2, final[K*N];
-    cudaMalloc((void **) &a1, N*K * sizeof(int));
-    cudaMalloc((void **) &a2, N*K * sizeof(int));
-
-    dim3 dim_block(BANK_SIZE, BANK_SIZE);
-    dim3 dim_grid((K - 1) / BANK_SIZE + 1, (N - 1) / BANK_SIZE + 1);
-
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-    cudaEventRecord(start, 0);
-    start_chrono = chrono::system_clock::now();
 
-//    gInit<<<numBlocks, blockSize>>>(a1);
-//    gCopy<<<numBlocks, blockSize>>>(a1, a2);
-    MatrixFill<<<dim_grid, dim_block>>>(a1);
-    TransMatrix<<<dim_grid, dim_block>>>(a1, a2);
-//    gInit<<<N,N>>>(a1);
-//    gCopy<<<N,N>>>(a1, a2);
+    cudaMalloc((void **) &GPU_pre_matrix, N * K * sizeof(int));
+    cudaMalloc((void **) &GPU_after_matrix, N * K * sizeof(int));
+    local_pre_matrix = (int *) calloc(N * K, sizeof(int));
+    local_after_matrix = (int *) calloc(N * K, sizeof(int));
 
-    cudaEventRecord(stop, 0);
-    end_chrono = chrono::system_clock::now();
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < K; ++j) {
+            local_pre_matrix[j + i * K] = j + i * K + 1;
+            cout << local_pre_matrix[j + i * K] << " ";
+        }
+        cout<< endl;
+    }
+
+    cout << "\ndim3((K + threads_per_block - 1) / threads_per_block = \n"
+         <<"x = " << dim3((K + threads_per_block - 1) / threads_per_block,(N + threads_per_block - 1) / threads_per_block).x << "\t"
+         <<"y = " << dim3((K + threads_per_block - 1) / threads_per_block,(N + threads_per_block - 1) / threads_per_block).y << "\t"
+         <<"z = " << dim3((K + threads_per_block - 1) / threads_per_block,(N + threads_per_block - 1) / threads_per_block).z << "\t\n"
+         << "dim3(threads_per_block, threads_per_block) = \n"
+         <<"x = " << dim3(threads_per_block, threads_per_block).x << "\t"
+         <<"y = " << dim3(threads_per_block, threads_per_block).y << "\t"
+         <<"z = " << dim3(threads_per_block, threads_per_block).z << endl;
+
+    cudaMemcpy(GPU_pre_matrix, local_pre_matrix, K * N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaEventRecord(start, nullptr);
+
+    gTransposition <<< dim3((K + threads_per_block - 1) / threads_per_block,(N + threads_per_block - 1) / threads_per_block),
+                       dim3(threads_per_block, threads_per_block)>>>(GPU_pre_matrix, GPU_after_matrix, N, K);
+
+    cudaDeviceSynchronize();
+
+    cudaEventRecord(stop, nullptr);
     cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&elapsedTime, start, stop);
+    cudaMemcpy(local_after_matrix, GPU_after_matrix, K * N * sizeof(float), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(final, a1, N*K * sizeof(int), cudaMemcpyDeviceToHost);
+    cout << endl;
+    for (long long i = 0; i < K; ++i) {
+        for (long long j = 0; j < N; ++j) {
+            cout << local_after_matrix[j + i * N] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    cudaEventElapsedTime(&elapsedTime, start, stop);
 
     cout << "CUDA Event time:\n\t"
          << elapsedTime
          << endl;
 
-    cout << "Chrono time:\n\t"
-         << chrono::duration_cast<ms>(end_chrono - start_chrono).count() << "ms\n\t"
-         << chrono::duration_cast<ns>(end_chrono - start_chrono).count() << "ns"
-         << endl;
-
-    cudaMemcpy(final, a1, N*K * sizeof(int), cudaMemcpyDeviceToHost);
-    cout << "Non transpose:  \n";
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < 10; j++) cout << " " << final[j + i * N];
-        cout << "\n";
-    }
-    cout << "\n";
-
-    cudaMemcpy(final, a2, N * sizeof(int), cudaMemcpyDeviceToHost);
-    cout << "Sorted: \n";
-    for(int i = 0 ; i < N ; i++){
-        for (int j = 0 ; j < 10 ; j ++) cout << " " << final[j + i * N];
-        cout << "\n";
-    }
-    cout << "\n";
-
-    cudaFree(a2);
-    cudaFree(a1);
+    cudaFree(GPU_pre_matrix);
+    cudaFree(GPU_after_matrix);
+    free(local_pre_matrix);
+    free(local_after_matrix);
 
     return 0;
 }
